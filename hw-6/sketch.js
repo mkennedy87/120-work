685eedf42c53e76461c18d3bcca8ef1efd6a60aa
Mike <michael1.kennedy@umontana.edu>
2019-03-12 03:18:38 -0600

    not bound to canvas yet

--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -1,16 +1,35 @@
+
+
 function setup(){
   createCanvas( 800, 450 );
   background( 100, 175, 255);
 }
 
 let circleSize = 20;
-
+let posX = 0;
+let posY = 0;
 
 function draw(){
   let pos_x = floor(random( 800 ) );
   let pos_y = floor(random( 450 ) );
   noStroke();
-  fill(  0, 0, 0, 10 );
+
+  fill(  0, 255, 0, 20 );
   ellipse( pos_x, pos_y, circleSize, circleSize );
+  let posGoX = 5
+  let posGoY = 5
+
+  posX += posGoX;
+  posY += posGoY;
+if( posX >= width || posX <= 0) {
+  posGoX = posGoX * -1;
+}
+if( posY >= height || posY <= 0) {
+  posGoY = posGoY * -1;
+}
+
+  fill( 255, 0, 0, 15 );
+  ellipse( posX, posY, 50, 50);
+
 
 }
