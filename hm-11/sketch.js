f0d3c7365902cf2541cf59c168f282f5d4493760
Mike <michael1.kennedy@umontana.edu>
2019-04-17 16:43:08 -0600

    decided the concept

--- hm-11/sketch.js
+++ hm-11/sketch.js
@@ -5,7 +5,7 @@ let bg_color;
 
 // create canvas size and color, also make for loop for array size
 function setup() {
-  createCanvas(windowWidth, 1000);
+  createCanvas(windowWidth, windowHeight);
   bg_color = color( 0, 100, 0 );
 
   for (let i = 0; i < num_of_objects; i++) {
@@ -21,4 +21,35 @@ function draw() {
   for (let i = 0; i < objects.length; i++) {
     objects[i].frame();
   }
+}
+
+// creating new class not yet sure what i'm making
+class ObjectBuild {
+  constructor() {
+    this.size_w_h = random(40, 150);
+    // this.size_h = random( 40, 150);
+    this.loc_x = random(width);
+    this.loc_y = random(height);
+    this.move_x = random( -10, 10);
+    this.move_y = random( -10, 10);
+    this.body_color = color( random(255), random(255), random(255));
+    this.size_w_h = this.size_w_h;
+
+  }
+
+// update objects
+frame() {
+  this.objectAngle();
+  this.display();
+  this.move();
+}
+
+display() {}
+
+push();
+
+translate(this.loc_x, this.loc_y);
+fill(this.body_color);
+// i've decided on squares, and i'll try a rotation
+rect( 0, 0, this.size_w_h);
 }
